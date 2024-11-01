d7c6ab439454fd87a066c154f7c3864935529bd0
jmartter <150058251+jmartter@users.noreply.github.com>
2024-11-01 10:53:41 +0100

    Initial commit

---
+++ src/main/java/org/example/gestionmagia/GestionMagiaApplication.java
@@ -0,0 +1,15 @@
+package org.example.gestionmagia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+// here we go again
+@SpringBootApplication
+public class GestionMagiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionMagiaApplication.class, args);
+    }
+
+}

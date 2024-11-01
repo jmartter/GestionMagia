02159ba7134344bb38f1d4076f60507d8cb3383a
jmartter <150058251+jmartter@users.noreply.github.com>
2024-11-01 11:51:30 +0100

    creacion usuario

---
+++ src/main/java/org/example/gestionmagia/Usuario/Usuario.java
@@ -0,0 +1,21 @@
+package org.example.gestionmagia.Usuario;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String contraseña;
+    private String correo;
+    private boolean administrador;
+}

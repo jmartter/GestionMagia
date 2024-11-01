cf9074e5746e8b5b9e0df04a9035c28b029c7b08
jmartter <150058251+jmartter@users.noreply.github.com>
2024-11-01 15:29:04 +0100

    Changes by nsfgo

--- src/main/java/org/example/gestionmagia/GestionMagiaApplication.java
+++ src/main/java/org/example/gestionmagia/GestionMagiaApplication.java
@@ -1,13 +1,29 @@
 package org.example.gestionmagia;
 
+import org.example.gestionmagia.Menu.Menu;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "org.example.gestionmagia")
 public class GestionMagiaApplication {
+
+    @Autowired
+    private Menu menu;
+
     public static void main(String[] args) {
         SpringApplication.run(GestionMagiaApplication.class, args);
     }
-}// dsfsdf
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            // Display the menu
+            menu.displayMenu();
+        };
+    }
+}

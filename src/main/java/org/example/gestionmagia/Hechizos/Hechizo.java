75fc060409cac6dd7cac0416d70fbdffbf454ebe
jmartter <150058251+jmartter@users.noreply.github.com>
2024-11-02 15:55:37 +0100

    Booof aqui no se toca mas , crear una nueva puta rama antes de liarla

--- src/main/java/org/example/gestionmagia/Hechizos/Hechizo.java
+++ src/main/java/org/example/gestionmagia/Hechizos/Hechizo.java
@@ -2,6 +2,8 @@ package org.example.gestionmagia.Hechizos;
 
 import org.example.gestionmagia.Usuario.Usuario;
 import org.example.gestionmagia.aspecto.Aspecto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -10,6 +12,8 @@ import java.util.concurrent.ExecutorService;
 
 @Component
 public class Hechizo {
+
+    private static final Logger logger = LoggerFactory.getLogger(Hechizo.class);
 
     @Autowired
     @Qualifier("singleThreadExecutor1")
@@ -36,24 +40,32 @@ public class Hechizo {
 
     public void lanzarHechizo1(Usuario usuario) {
         singleThreadExecutor1.submit(() -> {
+            String threadName = Thread.currentThread().getName();
+            logger.info("Ejecutando lanzarHechizo1 en el hilo: {} de la pool: singleThreadExecutor1", threadName);
             aspecto.captureThreadInfo("singleThreadExecutor1", usuario, 1);
         });
     }
 
     public void lanzarHechizo2(Usuario usuario) {
         singleThreadExecutor2.submit(() -> {
+            String threadName = Thread.currentThread().getName();
+            logger.info("Ejecutando lanzarHechizo2 en el hilo: {} de la pool: singleThreadExecutor2", threadName);
             aspecto.captureThreadInfo("singleThreadExecutor2", usuario, 2);
         });
     }
 
     public void lanzarHechizo3(Usuario usuario) {
         singleThreadExecutor3.submit(() -> {
+            String threadName = Thread.currentThread().getName();
+            logger.info("Ejecutando lanzarHechizo3 en el hilo: {} de la pool: singleThreadExecutor3", threadName);
             aspecto.captureThreadInfo("singleThreadExecutor3", usuario, 3);
         });
     }
 
     public void lanzarHechizo4(Usuario usuario) {
         singleThreadExecutor4.submit(() -> {
+            String threadName = Thread.currentThread().getName();
+            logger.info("Ejecutando lanzarHechizo4 en el hilo: {} de la pool: singleThreadExecutor4", threadName);
             aspecto.captureThreadInfo("singleThreadExecutor4", usuario, 4);
         });
     }
@@ -63,6 +75,8 @@ public class Hechizo {
         fixedThreadPool3.submit(() -> {
             try {
                 Thread.sleep(1000);
+                String threadName = Thread.currentThread().getName();
+                logger.info("Ejecutando lanzarHechizo1 en el hilo: {} de la pool: fixedThreadPool3", threadName);
                 aspecto.captureThreadInfo("fixedThreadPool3", usuario, 1);
                 lanzarHechizo1(usuario);
             } catch (InterruptedException e) {
@@ -73,6 +87,8 @@ public class Hechizo {
         fixedThreadPool3.submit(() -> {
             try {
                 Thread.sleep(1000);
+                String threadName = Thread.currentThread().getName();
+                logger.info("Ejecutando lanzarHechizo2 en el hilo: {} de la pool: fixedThreadPool3", threadName);
                 aspecto.captureThreadInfo("fixedThreadPool3", usuario, 2);
                 lanzarHechizo2(usuario);
             } catch (InterruptedException e) {
@@ -83,6 +99,8 @@ public class Hechizo {
         fixedThreadPool3.submit(() -> {
             try {
                 Thread.sleep(1000);
+                String threadName = Thread.currentThread().getName();
+                logger.info("Ejecutando lanzarHechizo3 en el hilo: {} de la pool: fixedThreadPool3", threadName);
                 aspecto.captureThreadInfo("fixedThreadPool3", usuario, 3);
                 lanzarHechizo3(usuario);
             } catch (InterruptedException e) {
